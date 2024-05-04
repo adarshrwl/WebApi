110d034048daf3c15c8856d45e5cf6729bd0ffca
Adarsh Rawal <adarshrawal321@gmail.com>
2024-05-04 21:11:04 +0545

    Added module for Appointment

---
+++ models/bookAppoinment.js
@@ -0,0 +1,15 @@
+const { default: mongoose } = require('mongoose');
+const Mongoose=require('mongoose');
+const bookAppoinment=new Mongoose.Schema({
+    date:{
+        type:date,
+        required:true,
+    },
+    time:{
+        type:time,
+        required:true,
+    }
+})
+
+const Appointment= mongoose.model("Appointment",bookAppoinment);
+module.exports=Appointment;
